3d88c7880b58b0c83b51c56cd75185934769b164
Raja <raja.gond@outlook.com>
2023-06-14 23:12:15 +0530

    MemCXLib: multi-process working?

--- emucxl_app.c
+++ emucxl_app.c
@@ -12,20 +12,9 @@ int main(int argc , char *argv[])
     int i;
     unsigned char *p_map;
 
-    // fd = open("/dev/emucxl", O_RDWR);
-    // if(fd < 0) {
-    //     perror("open");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // p_map = (unsigned char *)mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE,
-    //         MAP_SHARED, fd, 0);
-    // if(p_map == MAP_FAILED) {
-    //     printf("mmap fail\n");
-    //     goto here;
-    // }
     emucxl_init();
     // allocate
+    sleep(10);
     p_map = (unsigned char *)emucxl_alloc(PAGE_SIZE, LOCAL_MEMORY);
     printf("pmap address in user space memory : %p\n", p_map);
 
@@ -37,13 +26,18 @@ int main(int argc , char *argv[])
     for(i = 0; i < 10; i++)
         p_map[i] = i;
     printf("\n\n");
+    sleep(10);
+    // migrate 
+    p_map = (unsigned char *)emucxl_migrate((void*)p_map, REMOTE_MEMORY, PAGE_SIZE);
+    printf("pmap address in user space memory : %p\n", p_map);
+    for(i = 0; i < 10; i++)
+        printf("%d ", p_map[i]);
+    printf("\n\n");
 
-    // // migrate 
-    // p_map = (unsigned char *)emucxl_migrate((void*)p_map, REMOTE_MEMORY, PAGE_SIZE);
-    // printf("pmap address in user space memory : %p\n", p_map);
-    // for(i = 0; i < 10; i++)
-    //     printf("%d ", p_map[i]);
-    // printf("\n\n");
+    printf("\n Write data \n");
+    for(i = 0; i < 10; i++)
+        p_map[i] = i*2;
+    printf("\n\n");
 
     // resize
     p_map = (unsigned char *)emucxl_resize((void*)p_map, LOCAL_MEMORY, PAGE_SIZE, PAGE_SIZE);
--- script.sh
+++ script.sh
@@ -3,6 +3,8 @@
 make clean
 make
 sudo insmod emucxl_mmap.ko
+sudo ./emucxl_app &
+sleep 1
 sudo ./emucxl_app
 sudo rmmod emucxl_mmap
 sudo dmesg | tail -20
