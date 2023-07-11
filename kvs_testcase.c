3245a12fbde7297bb0a7d4abc9a8265a16bb9d52
Raja <raja.gond@outlook.com>
2023-07-11 23:52:17 +0530

    emucxl: kv experiment

--- kvs_testcase.c
+++ kvs_testcase.c
@@ -25,26 +25,51 @@ int main(int argc, char *argv[]){
         sprintf(value, "value%d", i);
         kv_store_put(&store, key, value);
     }
-
-    for (int i = 0; i < 40000; i++) {
-        int num = (rand() % 1000 + 1);
-        char key[10];
-        sprintf(key, "key%d", num);
-        const char* value = kv_store_get(&store, key);
-        if (value == NULL) {
-            printf("Key %s not found\n", key);
+    int x = atoi(argv[3]);
+    if (x == 0)
+    {
+        // random get requests
+        for (int i = 0; i < 50000; i++) {
+            int num = (rand() % 1000 + 1);
+            char key[10];
+            sprintf(key, "key%d", num);
+            const char* value = kv_store_get(&store, key);
+            if (value == NULL) {
+                printf("Key %s not found\n", key);
+            }
         }
+        printf("Local fetch count:\t %d\n", store.local_fetch_count);
+        printf("Remote fetch count:\t %d\n", store.remote_fetch_count);
+        printf("Random get requests done\n");
     }
-    
-    // for (int i = 6; i <= 10; i++) {
-    //     char key[10];
-    //     sprintf(key, "key%d", i);
-    //     printf("Delete key %s\n", key);
-    //     kv_store_delete(&store, key);
-    // }
+    else
+    {
+        int k = x * 10;
+        // x% object gets 90% get requests
+        for(int i = 1; i <= 45000; i++){
+            int num = (rand() % k + 1);
+            char key[10];
+            sprintf(key, "key%d", num);
+            const char* value = kv_store_get(&store, key);
+            if (value == NULL) {
+                printf("Key %s not found\n", key);
+            }
+        }
 
-    printf("Local fetch count:\t %d\n", store.local_fetch_count);
-    printf("Remote fetch count:\t %d\n", store.remote_fetch_count);
+        // other 10% get requests
+        for (int i = 1; i <= 5000; i++) {
+            int num = (rand() % (1000 - k) + (k + 1));
+            char key[10];
+            sprintf(key, "key%d", num);
+            const char* value = kv_store_get(&store, key);
+            if (value == NULL) {
+                printf("Key %s not found\n", key);
+            }
+        }
+        printf("Local fetch count:\t %d\n", store.local_fetch_count);
+        printf("Remote fetch count:\t %d\n", store.remote_fetch_count);
+        printf("%d%% object gets 90%% get requests done\n", x);
+    }
 
     kv_store_destroy(&store);
 
--- script.sh
+++ script.sh
@@ -10,11 +10,21 @@ sudo insmod emucxl_kernel.ko
 # sudo rmmod emucxl_kernel
 # # sudo dmesg | tail -20
 # python3 graph.py
-for i in 1 10 25 37 45
+# for i in 1 10 25 37 45
+# do
+#     echo "Running seed $i"
+#     sudo ./kvs_testcase 1 $i
+#     sudo ./kvs_testcase 0 $i
+# done
+
+echo "Policy 1: Fetch object to local if it is in remote"
+echo "Policy 0: Keep object in remote if it is in remote"
+
+for i in 0 10 20 30 40 50 60 70 80 90
 do
-    echo "Running seed $i"
-    sudo ./kvs_testcase 1 $i
-    sudo ./kvs_testcase 0 $i
+    echo "x = $i"
+    sudo ./kvs_testcase 1 1 $i
+    sudo ./kvs_testcase 0 1 $i
 done
 
 sudo rmmod emucxl_kernel
